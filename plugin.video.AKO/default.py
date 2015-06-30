20840187716da2cdb012c39f4049ee4c821dd5e3
HalowTv <halow@live.se>
2015-06-30 20:14:31 +0200

    uPDATE KarwanTV to 2.3

--- plugin.video.AKO/default.py
+++ plugin.video.AKO/default.py
@@ -1,16 +1,21 @@
 # -*- coding: iso-8859-1 -*-
-# please visit http://halow.x10.bz
+# please visit 
 
 import xbmc,xbmcgui,xbmcplugin,sys,urllib2
+import resources.lib.requests as requests
 from resources.lib.BeautifulSoup import BeautifulStoneSoup, BeautifulSoup, BeautifulSOAP
 
-icons = "http://karwan.tv/images/tvlogo/"
-icon = xbmc.translatePath("special://home/addons/plugin.video.Karwan-kurdtv/icon.png")
+if 'extrafanart' in sys.argv[2]: sys.exit(0)
+
+icons = "687474703A2F2F6B617277616E2E74762F696D616765732F74766C6F676F2F"
+icon = xbmc.translatePath("special://home/addons/plugin.video.iptvxtra-kurdtv/icon.png")
 plugin_handle = int(sys.argv[1])
+md5 = 'hex'
 mode = sys.argv[2]
 	
 def add_video_item(url, title, img=''):
-    url = 'plugin://plugin.video.AKO/?playkurd=' + url + '***' + title + '***' + img
+    #xba = (url + '***' + title + '***' + img).encode(md5)
+    url = 'plugin://plugin.video.AKO/?playkurd=' + url.encode(md5) + '***' + title + '***' + img
     listitem = xbmcgui.ListItem(title, iconImage=img, thumbnailImage=img)
     listitem.setInfo( type="Video", infoLabels={ "Label": title, "Title": title } )
     listitem.setProperty('IsPlayable', 'false')
@@ -22,15 +27,14 @@ def iptvxtra_play():
     xbmc.executebuiltin('XBMC.Notification('+idx[1]+' , KARWANTV ,5000,'+idx[2]+')')
 
     if idx[0] <> '':
-        import resources.lib.requests as requests
-        xxx = idx[0]
+        xxx = idx[0].decode(md5)
         ref = xxx.replace('live/','').replace('php','html')
         r = requests.get(xxx, headers={"Referer": ref})
         r = r.text.strip().replace('\n',' ')
         xxx = find_between(r,'file:',',')
         idx[0] = xxx.replace(' ','').replace("'","").replace('"','').replace('}','').replace('manifest.f4m','playlist.m3u8').strip()
-        if 'rtmp://' in idx[0]:
-            idx[0] = idx[0] + ' swfUrl=http://p.jwpcdn.com/6/11/jwplayer.flash.swf pageUrl=http://karwan.tv live=1'
+        iudy = '73776655726C3D687474703A2F2F702E6A777063646E2E636F6D2F362F31312F6A77706C617965722E666C6173682E737766207061676555726C3D687474703A2F2F6B617277616E2E7476206C6976653D31'
+        if 'rtmp://' in idx[0]: idx[0] = idx[0] + ' ' + iudy.decode(md5)
 
     xbmcPlayer = xbmc.Player()
     listitem = xbmcgui.ListItem( idx[1], iconImage=idx[2], thumbnailImage=idx[2])
@@ -48,17 +52,21 @@ def find_between(s,first,last):
     except ValueError:
         return ""
 
-def get_url():
-        req = urllib2.Request('http://srv1.iptvxtra.net/xbmc/xml/kurdistan.xml')
+def auth():
+    try:
+        payload = {'loc': '6B757264697374616E','h':'0','sh':'servhost','la':'Q'}
+        r = requests.get("http://www.iptvxtra.net/xbmc/_form/rsx.php", params=payload)
+        linx = r.text.replace('.','7').replace('/','A').decode(md5).split('***')
+        req = urllib2.Request(linx[0])
+        req.add_header('Referer', linx[1])
         response = urllib2.urlopen(req)
-        link=response.read()
-        return link
+        frame = response.read()
+    except: frame = ''
+    return frame
 
 def main():
 	
-    link=get_url()
-	
-    soup = BeautifulSOAP(link, convertEntities=BeautifulStoneSoup.XML_ENTITIES)
+    soup = BeautifulSOAP(auth(), convertEntities=BeautifulStoneSoup.XML_ENTITIES)
     items = soup.findAll("item")
     for item in items:
             try:
@@ -77,5 +85,6 @@ def main():
 
 if 'playkurd' in mode:
     iptvxtra_play()
+    sys.exit(0)
 else:
     main()
